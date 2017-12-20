16899431f2e3b84d408765302919bae4f6a0fee6
Lee Richardson <github@leerichardson.com>
2017-12-19 19:15:19 -0500

    A clean command

---
+++ Tools/clean.bat
@@ -0,0 +1,4 @@
+rmdir /s /q ..\Build\arm\SirenOfShame
+REM del ..\Build\arm\pkgs\AutomatedArchitecture.Appx.SosBackground.cab
+REM del ..\Build\arm\pkgs\AutomatedArchitecture.Appx.SosUi.cab
+REM del ..\Build\arm\pkgs\AutomatedArchitecture.Appx.SosRelay.cab
